b9adc97fb37020d415708f61778294e605ad5203
GORKEM1986 <aslanakbey7106@gmail.com>
2023-11-24 19:55:46 +0100

    tekrar yüklendi

---
+++ 19.11.2023/8-Ilyas/Javascript/script.js
@@ -0,0 +1,19 @@
+const ad = 'ilyas';
+const soyad = 'Kaya';
+var x= ad + ''+ soyad
+console.log(x);
+
+
+
+let a=0 ;
+
+do {
+    console.log (a);
+    a=a+2;
+    }
+    
+    while a<=100;
+    {
+        console.log ('0 da 100 e cift sayi toplami hesaplandi');
+    }
+
