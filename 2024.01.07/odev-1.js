223acc166385975f1f2e7fdc5b63bc741493d3a9
murat <merdo.net@gmail.com>
2024-01-07 17:29:12 +0100

    odev

---
+++ 2024.01.07/odev-1.js
@@ -0,0 +1,56 @@
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: 202,
+    mass: 136,
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: 150,
+    mass: 49,
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: 188,
+    mass: 84,
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+
+// MAP
+// 1. Tüm isimlerin dizisini al
+// 2. Tüm kisilerin boylarinin dizisini al
+// 3. Tüm kisilerin agirliklarinin dizisini al
+// 4. Yalnizca adi ve göz reklerini iceren dizi olustur
+
+// REDUCE
+// 1. Tüm kisilerin toplam agirliklari
+// 2. Tüm kisilerin toplam boy uzunluklari
+// 1. Tüm kisilerin ortalama agirliklari
+// 2. Tüm kisilerin ortalama boy uzunluklari
+
+// FILTER
+// 1. Agirligi 100 den kücük olan kisiler
+// 2. Tüm erkek karakterler
+
+// SORT
+// 1. Uzunluga göre sirala A-Z
+// 2. Kiloya göre sirala Z-A
+
+// EVERY
+// 1. Hepsinin gözü mavi mi?
+
+// SOME
+// 1. En ay bir kisinin gözü mavi mi?
