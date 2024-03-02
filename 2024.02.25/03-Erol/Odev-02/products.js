e743e4a3dd2a38b36bb40f8305a72ce7e680d885
Erol Ertekin <68508815+erolertekinn@users.noreply.github.com>
2024-03-02 11:49:57 +0100

    Node.js Odevi - Düzeltmeye devam edicem

---
+++ 2024.02.25/03-Erol/Odev-02/products.js
@@ -0,0 +1,7 @@
+export const productList = [
+  { name: "ekmek", price: "0.50$" },
+  { name: "su", price: "0.50$" },
+  { name: "patates", price: "1.50$" },
+  { name: "domates", price: "2.50$" },
+  { name: "sut", price: "1.00$" },
+];
