8814559ac3ff2f82620f36dfac4ace031c054068
GORKEM1986 <aslanakbey7106@gmail.com>
2024-03-09 15:48:03 +0100

    güncel

---
+++ 2024.02.25/04-Fatih_nodejs/odev_2/productList.js
@@ -0,0 +1,8 @@
+export default [
+  { name: "ekmek", price: "1.00$" },
+  { name: "sut", price: "1.50$" },
+  { name: "su", price: "1.30$" },
+  { name: "patates", price: "3.00$" },
+  { name: "domates", price: "3.50$" },
+];
+ 
