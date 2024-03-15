3c90ae7c76f348a99c84a0796ee9b0a2c29e387d
Yücel Özen <149947864+OZEN1112@users.noreply.github.com>
2024-03-15 15:02:15 +0100

    Add files via upload

--- 2024.03.10/calc.js
+++ 2024.03.10/calc.js
@@ -1,43 +1,56 @@
 export function calc(sayi_1, sayi_2, islem) {
-  var result = {
-    sonuc: 0,
-    hata: "",
-  };
+    var result = {
+      sonuc: 0,
+      hata: "",
+    };
 
-  if (typeof sayi_1 !== "number" || typeof sayi_2 !== "number") {
-    result.hata = "Lütfen sayi giriniz.";
+    if ( islem === "topla"){
+      islem ="+"
+
+    }else if (islem ==="cikar"){
+      islem ="-"
+    }else if (islem === "carp"){
+      islem ="*"
+    }else if (islem === "böl"){
+      islem ="/"
+    }else{
+      result.hata = "gecersiz bir islem girdiniz";
+      return result;
+    }
+  
+    if (typeof sayi_1 !== "number" || typeof sayi_2 !== "number") {
+      result.hata = "Lütfen sayi giriniz.";
+      return result;
+    }
+  
+    if (islem == "/" && sayi_2 == 0) {
+      result.hata = "Bölme isleminde ikinci sayi sifir olamaz.";
+      return result;
+    }
+  
+    if (["+", "-", "/", "*"].indexOf(islem) == -1) {
+      result.hata = "Hatali islem operatörü.";
+      return result;
+    }
+  
+    switch (islem) {
+      case "+":
+        result.sonuc = sayi_1 + sayi_2;
+        break;
+      case "-":
+        result.sonuc = sayi_1 - sayi_2;
+        break;
+      case "/":
+        result.sonuc = sayi_1 / sayi_2;
+        break;
+      case "*":
+        result.sonuc = sayi_1 * sayi_2;
+        break;
+  
+      default:
+        result.sonuc = 0;
+        break;
+    }
+  
     return result;
   }
-
-  if (islem == "/" && sayi_2 == 0) {
-    result.hata = "Bölme isleminde ikinci sayi sifir olamaz.";
-    return result;
-  }
-
-  if (["+", "-", "/", "*"].indexOf(islem) == -1) {
-    result.hata = "Hatali islem operatörü.";
-    return result;
-  }
-
-  switch (islem) {
-    case "+":
-      result.sonuc = sayi_1 + sayi_2;
-      break;
-    case "-":
-      result.sonuc = sayi_1 - sayi_2;
-      break;
-    case "/":
-      result.sonuc = sayi_1 / sayi_2;
-      break;
-    case "*":
-      result.sonuc = sayi_1 * sayi_2;
-      break;
-
-    default:
-      result.sonuc = 0;
-      break;
-  }
-
-  return result;
-}
-
