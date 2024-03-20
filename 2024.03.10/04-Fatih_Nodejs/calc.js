f0f3894e4c8591112c1628d554dbd4d7ad8cf4c5
GORKEM1986 <aslanakbey7106@gmail.com>
2024-03-20 12:03:24 +0100

    nodejs

---
+++ 2024.03.10/04-Fatih_Nodejs/calc.js
@@ -0,0 +1,55 @@
+export function calc(sayi_1, sayi_2, islem) {
+  var result = {
+    sonuc: 0,
+    hata: "",
+  };
+
+  if (islem === "topla") {
+    islem = "+";
+  } else if (islem === "cikar") {
+    islem = "-";
+  } else if (islem === "bol") {
+    islem = "/";
+  } else if (islem === "carp") {
+    islem = "*";
+  } else {
+    result.hata = "Lütfen sayı giriniz";
+    return result;
+  }
+
+  if (typeof sayi_1 !== "number" || typeof sayi_2 !== "number") {
+    result.hata = "Lütfen sayi giriniz.";
+    return result;
+  }
+
+  if (islem == "/" && sayi_2 == 0) {
+    result.hata = "Bölme isleminde ikinci sayi sifir olamaz.";
+    return result;
+  }
+
+  if (["+", "-", "/", "*"].indexOf(islem) == -1) {
+    result.hata = "Hatali islem operatörü.";
+    return result;
+  }
+
+  switch (islem) {
+    case "+":
+      result.sonuc = sayi_1 + sayi_2;
+      break;
+    case "-":
+      result.sonuc = sayi_1 - sayi_2;
+      break;
+    case "/":
+      result.sonuc = sayi_1 / sayi_2;
+      break;
+    case "*":
+      result.sonuc = sayi_1 * sayi_2;
+      break;
+
+    default:
+      result.sonuc = 0;
+      break;
+  }
+
+  return result;
+}
