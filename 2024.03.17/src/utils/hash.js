0d487b8f10e8fd73cfc85977baa0855da77d34db
ozlemakcicek <ozlemakcicek83@gmail.com>
2024-04-28 22:10:20 +0200

    2024.03.17 klasörü silindi

--- 2024.03.17/src/utils/hash.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export const hash = (pass) => {
-  return bcrypt.hashSync(pass, bcrypt.genSaltSync(10), (err, hash) => {
-    if (err) throw err;
-
-    pass = hash;
-  });
-};
